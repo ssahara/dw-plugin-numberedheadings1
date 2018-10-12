0e08e2359ffa176e431d7dd590474d7ee3f49910
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-10-12 23:23:09 +0900

    plugin mode property

--- syntax.php
+++ syntax.php
@@ -49,17 +49,21 @@ class syntax_plugin_numberedheadings extends DokuWiki_Syntax_Plugin {
     /**
      * Connect pattern to lexer
      */
+    protected $mode;
+
+    function preConnect() {
+        // syntax mode, drop 'syntax_' from class name
+        $this->mode = substr(get_class($this), 7);
+    }
+
     function connectTo($mode) {
-        $this->Lexer->addSpecialPattern( '{{header>[1-5]}}',
-                                         $mode,
-                                         'plugin_numberedheadings');
-        // added new parameter (matches the parameter name for better recognition)
-        $this->Lexer->addSpecialPattern( '{{startlevel>[1-5]}}',
-                                         $mode,
-                                         'plugin_numberedheadings');
-        $this->Lexer->addSpecialPattern( '^[ \t]*={2,6}\s?\-[^\n]+={2,6}[ \t]*(?=\n)',
-                                         $mode,
-                                         'plugin_numberedheadings');
+        $this->Lexer->addSpecialPattern(
+                        '{{header>[1-5]}}', $mode, $this->mode);
+        $this->Lexer->addSpecialPattern(
+                        '{{startlevel>[1-5]}}', $mode, $this->mode);
+
+        $this->Lexer->addSpecialPattern(
+                        '^[ \t]*={2,6}\s?\-[^\n]+={2,6}[ \t]*(?=\n)', $mode, $this->mode);
     }
 
     /**
